2e4711320db458c7946796843e5111abf60273d3
LoveCode-GitHub <guohaitao90@gmail.com>
2019-10-03 09:30:56 +0800

    Site updated: 2019-10-03 09:30:54

--- js/utils.js
+++ js/utils.js
@@ -23,7 +23,7 @@ NexT.utils = {
     document.querySelectorAll('.post-body :not(a) > img').forEach(element => {
       var $image = $(element);
       var imageLink = $image.attr('data-src') || $image.attr('src');
-      var $imageWrapLink = $image.wrap(`<a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent('a');
+      var $imageWrapLink = $image.wrap(`<a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="https://schema.org/ImageObject" itemprop="url"></a>`).parent('a');
       if ($image.is('.post-gallery img')) {
         $imageWrapLink.addClass('post-gallery-img');
         $imageWrapLink.attr('data-fancybox', 'gallery').attr('rel', 'gallery');
